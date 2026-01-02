bcc7a9ef5d36392c5a795f93f19f4d64d423fb63
onanamichel47-pixel <onanamichel47@gmail.com>
2026-01-02 23:59:04 +0100

    backend LC 6

--- models/index.js
+++ models/index.js
@@ -165,7 +165,7 @@ const initPermissions = async () => {
 
 const syncDatabase = async () => {
   try {
-    await sequelize.sync({ force: true });
+    await sequelize.sync({ true: true });
     console.log('✅ Tables synchronisées (Postgres)');
     await initPermissions();
     console.log('✅ Permissions initialisées');
